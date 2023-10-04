cbc2e3f62c25e76307c2aad37b3b2367a280c107
Jtrejos2901 <97121551+Jtrejos2901@users.noreply.github.com>
2023-10-04 15:18:55 -0600

    feat: Se generan más gráficos.

--- main.R
+++ main.R
@@ -2,23 +2,28 @@ library(openxlsx)
 library(purrr)
 library(tidyr)
 library(ggplot2)
+library(readxl)
 
-Ruta<- ""
-hojas = getSheetNames(Ruta)
+#Obtención de información:
 
-Ruta2<- ""
-hojas2 = getSheetNames(Ruta2)
+#Ruta<- ""
+#hojas = getSheetNames(Ruta)
+
+#Ruta2<- ""
+#hojas2 = getSheetNames(Ruta2)
+
+#nombres_hojas <- excel_sheets("")
 
 
-lista_df = map(hojas,function(x){
-  read.xlsx(Ruta ,sheet = x)
-})
+# lista_df = map(hojas,function(x){
+#   read.xlsx(Ruta ,sheet = x)
+# })
+# 
+# lista_df2 = map(hojas2,function(x){
+#   read.xlsx(Ruta2 ,sheet = x)
+# })
 
-lista_df2 = map(hojas2,function(x){
-  read.xlsx(Ruta2 ,sheet = x)
-})
-
-#Funciones
+#Funciones:
 
 tables<-function(table){
   condiciones <- (length(table[[1]]) / 8)
@@ -53,19 +58,17 @@ tables<-function(table){
   return(result) 
 }
 
+# for(i in 1:length(lista_df)){
+#   a<-tables(lista_df[[i]])
+#   b<- hojas[i]
+#   print(b)
+#   print(a)
+#   print(chisq.test(a))
+# }
 
-for(i in 1:length(lista_df)){
-  a<-tables(lista_df[[i]])
-  b<- hojas[i]
-  print(b)
-  print(a)
-  print(chisq.test(a))
-}
 
-#Gráficos
 
-#Reformar datos
-
+#Reformar datos:
 
 for (i in lista_df2) {
   datos_long <- gather(i, Variable, Valor, -Año,-Trimestre, -Condicion)
@@ -82,7 +85,7 @@ for (i in lista_df2) {
   
 }
 
-
+#Gráficos
 #ggplot(datos_long, aes(x = factor(Condicion), y = Valor, color = as.factor(Año))) +
 # geom_point() + 
 #labs(title = "Evolución de Condiciones por Año",
@@ -101,3 +104,31 @@ for (i in lista_df2) {
 #                                   y = "Valor", shape = "Trimestre") +
 #scale_color_discrete(name = "Año") +
 #facet_grid(~Variable)
+
+
+
+lista_df = map(hojas,function(x){
+  read.xlsx(Ruta ,sheet = x)
+})
+
+
+colores_personalizados <- c("Sin pareja" = "purple", "Con pareja" = "pink", 
+                            "Sin hijos" = "purple", "Con hijos" = "pink", 
+                            "0 a 5" = "purple", "6 a 14 " = "pink", 
+                            "6 a 14" = "pink", "15 o más" = "skyblue")
+
+for (i in lista_df){
+  datos_long5 <- gather(i, Variable, Valor, -Año,-Trimestre, -Condicion)
+  
+  plot5 <- ggplot(datos_long5, aes(x = factor(Variable), y = Valor, fill = Condicion)) +
+    geom_boxplot() +
+    scale_fill_manual(values = colores_personalizados) +
+    labs(
+      title = "Grafico de cajas y bigotes",
+      x = "Variable",
+      y = "Cantidad de Mujeres"
+    ) + 
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  plot_int5 <- ggplotly(plot5)
+  print(plot_int5)
+}
